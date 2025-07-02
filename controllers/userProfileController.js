06e1795056c6a93cb031909c2156cd01eaac7d80
Muna-Zeer <munaelzeer@gmail.com>
2025-07-02 09:47:20 +0300

    coding get user with details info

--- controllers/userProfileController.js
+++ controllers/userProfileController.js
@@ -1,54 +1,68 @@
 import User from "../database/models/users.js";
 import bcrypt from "bcrypt";
+class UserProfileController {
+  createUser = async (req, res) => {
+    try {
+      const {
+        firstName,
+        lastName,
+        email,
+        password,
+        confirmedPassword,
+        DOP,
+        image,
+      } = req.body;
+      if (!firstName || !email || !password || !confirmedPassword) {
+        return res.status(400).json({ message: "Enter empty fields" });
+      }
+      if (password != confirmedPassword) {
+        return res
+          .status(400)
+          .json({ message: "Password doesn't match confirmed password" });
+      }
 
-export const createUser = async (req, res) => {
-  try {
-    const {
-      firstName,
-      lastName,
-      email,
-      password,
-      confirmedPassword,
-      DOP,
-      image,
-    } = req.body;
-    if (!firstName || !email || !password || !confirmedPassword) {
-      return res.status(400).json({ message: "Enter empty fields" });
-    }
-    if (password != confirmedPassword) {
-      return res
-        .status(400)
-        .json({ message: "Password doesn't match confirmed password" });
-    }
+      const existingUser = await User.findOne({ where: { email } });
+      if (existingUser) {
+        return res.status(400).json({ message: "email already existing" });
+      }
+      const hashedPassword = await bcrypt.hash(password, 10);
+      const newUser = await User.create({
+        firstName,
+        lastName,
+        email,
+        password,
+        password: hashedPassword,
+        confirmedPassword: hashedPassword,
+        DOP,
+        image,
+      });
 
-    const existingUser = await User.findOne({ where: { email } });
-    if (existingUser) {
-      return res.status(400).json({ message: "email already existing" });
+      const userResponse = newUser.toJSON();
+      delete userResponse.password;
+    } catch (error) {
+      return res.status(500).json({
+        message: `Error creating user ${error}`,
+      });
     }
-    const hashedPassword = await bcrypt.hash(password, 10);
-    const newUser = User.create({
-      firstName,
-      lastName,
-      email,
-      password,
-      password: hashedPassword,
-      confirmedPassword: hashedPassword,
-      DOP,
-      image,
-    });
+  };
+    getUser=async(req,res)=>{
+    try {
+        const {id}=req.params;
+        const user = User.findByPk(id,{
+           attributes:{exclude:[password]},
+        });
 
-    const {
-      password: _,
-      confirmedPassword: __,
-      ...userResponse
-    } = User.toJSON();
-    return res.status(201).json({
-      message: "Successfully created new user",
-      user: userResponse,
-    });
-  } catch (error) {
-    return res.status(500).json({
-      message: `Error creating user ${error}`,
-    });
-  }
-};
+        if(!user){
+            return res.status(404).json({
+                message:"User not found"
+            });
+        }
+        return res.status(200).json({message:"get user successfully",user});
+
+    } catch (error) {
+        return res.status(500).json({message:`Internal server error ${error}`});
+    }
+   }
+}
+
+export const userProfileController = new UserProfileController();
