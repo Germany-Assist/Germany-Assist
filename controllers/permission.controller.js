e1cd5d67bc0571a0175834d16c1b6bf78843ac25
Amr imad <amrimadkh@gmail.com>
2025-08-13 18:03:35 +0300

    updating permission controller

--- controllers/permission.controller.js
+++ controllers/permission.controller.js
@@ -1,9 +1,19 @@
+import {
+  adminPermissions,
+  rootBusinessPermissions,
+} from "../database/templates.js";
 import * as permissionServices from "../services/permission.services.js";
+import { AppError } from "../utils/error.class.js";
 export async function assignPermission(req, res, next) {
   try {
-    console.log("success");
-    // const { userId, permissionId } = req.body;
-    // permissionServices.adjustPermission(userId, permissionId, "assign");
+    // to maintain security the templates only will be followed
+    let allowedPermissions;
+    const { userId, permissionId } = req.body;
+    if (req.auth.role === "root") allowedPermissions = rootBusinessPermissions;
+    if (req.auth.role === "admin") allowedPermissions = adminPermissions;
+    if (!allowedPermissions.includes(permissionId))
+      throw new AppError(403, "unAuthorized", true, "unAuthorized");
+    permissionServices.adjustPermission(userId, permissionId, "assign");
     res.sendStatus(200);
   } catch (error) {
     next(error);
@@ -12,8 +22,14 @@ export async function assignPermission(req, res, next) {
 
 export async function revokePermission(req, res, next) {
   try {
+    let allowedPermissions;
     const { userId, permissionId } = req.body;
+    if (req.auth.role === "root") allowedPermissions = rootBusinessPermissions;
+    if (req.auth.role === "admin") allowedPermissions = adminPermissions;
+    if (!allowedPermissions.includes(permissionId))
+      throw new AppError(403, "unAuthorized", true, "unAuthorized");
     permissionServices.adjustPermission(userId, permissionId, "revoke");
+    res.sendStatus(200);
   } catch (error) {
     next(error);
   }
