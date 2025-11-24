4023037ec6d43b66f1386970b09b28a8af0cf422
Amr Imad <amrimadkh@gmail.com>
2025-11-24 23:49:01 +0200

    fixing the scripts

--- database/dbIndex.js
+++ database/dbIndex.js
@@ -17,10 +17,13 @@ import Timeline from "./models/timeline.js";
 import Post from "./models/post.js";
 import Comment from "./models/comment.js";
 import AssetTypes from "./models/assetTypes.js";
-import { NODE_ENV } from "../configs/serverConfig.js";
 import Notification from "./models/notification.js";
+import { NODE_ENV } from "../configs/serverConfig.js";
+import { sequelize } from "./connection.js";
 
 export const defineConstrains = () => {
+  if (sequelize.associationsDefined) return;
+  sequelize.associationsDefined = true;
   //notification
   Notification.belongsTo(User, { foreignKey: "user_id" });
   User.hasMany(Notification, { foreignKey: "user_id" });
@@ -171,9 +174,9 @@ export const defineConstrains = () => {
 
   return true;
 };
-if (NODE_ENV !== "test") {
-  defineConstrains();
-}
+
+defineConstrains();
+
 const db = {
   User,
   AssetTypes,
