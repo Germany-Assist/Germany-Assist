1b9d55baabd9fe5a9c091ac373c7b618699a0a31
Amr Imad <amrimadkh@gmail.com>
2026-01-16 17:13:44 +0200

    adding cron for timeline closing

--- src/cron/timelines.cron.js
+++ src/cron/timelines.cron.js
@@ -3,29 +3,37 @@ import db from "../database/index.js";
 import { Op, Sequelize } from "sequelize";
 import { sequelize } from "../configs/database.js";
 import { errorLogger, infoLogger } from "../utils/loggers.js";
+
 const testDuration = "0 * * * * *";
 const actualDuration = "* * 0 * * *";
-const timelinesClosingCron = cron.schedule("0 0 0 * * *", async () => {
+const timelinesClosingCron = cron.schedule("*/3 * * * * *", async () => {
   const transaction = await sequelize.transaction();
   try {
-    const update = await db.Order.update(
+    const updated = await db.Order.update(
       { status: "pending_completion" },
       {
         where: {
-          id: orderId,
           status: "active",
-          type: "timeline",
-          serviceProviderId,
+          relatedType: "timeline",
+          relatedId: {
+            [Op.in]: Sequelize.literal(`(
+          SELECT id FROM "timelines"
+          WHERE "end_date" <= NOW()
+        )`),
+          },
         },
+
         raw: true,
         transaction,
+        returning: true,
       }
     );
-    infoLogger(`No closed Order ready to move`);
+    console.log(updated);
+    infoLogger(`No timeline was closed`);
     await transaction.commit();
   } catch (err) {
     await transaction.rollback();
-    errorLogger("Payout cron failed:", err);
+    errorLogger("timeline cron failed:", err);
   }
 });
 
